b29ba6b0303aef0e21b341906a72a745ebc08821
andreidobrinski <andreidobrinski@users.noreply.github.com>
2024-02-24 20:03:55 +0000

    deploy: 3b03637c39feef67cd70ee0cb2b7c91015f7fe32

--- _app/immutable/chunks/paths.REHq1car.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_hq8kyf?.base??"/scratchpad",a=globalThis.__sveltekit_hq8kyf?.assets??s;export{a,s as b};
---
+++ _app/immutable/chunks/paths.Yi3h786I.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1h6mbn9?.base??"/scratchpad",a=globalThis.__sveltekit_1h6mbn9?.assets??s;export{a,s as b};
